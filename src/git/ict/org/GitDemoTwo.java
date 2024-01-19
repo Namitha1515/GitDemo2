2ac87b1550c57fd6de335f5611be1b77cf25eab4
my <my@DESKTOP-MJJQUKN>
2024-01-19 18:04:20 +0530

    Second Commit

---
+++ src/git/ict/org/GitDemoTwo.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package git.ict.org;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class GitDemoTwo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Please enter your message.");
+String text=sc.nextLine();
+		System.out.println("The users message is "+text);
+				System.out.println("please enter your fav number");
+		int num1=sc.nextInt();
+		System.out.println("The users fav number is "+num1);
+
+	}
+
+}
